2628234e7593eca1cfa5fe30d0db2d2286bbff53
shaolong.xu <shaolong.xu@fengjr.com>
2016-09-01 09:22:27 +0800

    404跳转

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -19,7 +19,7 @@ class ErrorController extends Yaf_Controller_Abstract
         $exception = $this->getRequest()->getException();
 
         // 此处可扩展，当接口遇到错误并停止时，会写条日志的
-        Log_Log::info('error:' . $exception->getMessage(), true, true, 'error_log');
+        Log_Log::info('error:' . $exception->getMessage() .'|file:'.$exception->getFile().'|line:'.$exception->getLine(), true, true, 'error_log');
 
         if (isset($_GET['x'])) {
             echo "<pre>";
@@ -29,6 +29,7 @@ class ErrorController extends Yaf_Controller_Abstract
 
         // 加载失败
         if ($exception instanceof Yaf_Exception_LoadFailed) {
+            $this->redirect('/404');return;
 
             // 其他错误
         } else {
